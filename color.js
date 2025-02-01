13e8c45f972dc35efc8e942522c559f8bbd8388c
bbBSODdd <3348640434@qq.com>
2025-02-01 18:56:33 +0800

    202502011856

--- color.js
+++ color.js
@@ -1,1 +1,16 @@
-mdui.setColorScheme('#9FB1F1');
+var color_in_ls = localStorage.getItem("wbms_color");
+if (color_in_ls == null){
+    mdui.setColorScheme('#9FB1F1');
+    document.getElementById('cc').value = '#9FB1F1';
+    localStorage.setItem('wbms_color', '#9FB1F1');
+}else{
+    mdui.setColorScheme(color_in_ls);
+}
+function setcc(){
+    document.getElementById('cc').value = color_in_ls;
+}
+function colorchange(){
+    localStorage.setItem('wbms_color', document.getElementById('cc').value);
+    color_in_ls = localStorage.getItem("wbms_color");
+    mdui.setColorScheme(color_in_ls);
+}
